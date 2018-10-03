c34d865a769d9768c037fd2379f0b014f9f9654d
Matthew Campbell <cattmampbell@gmail.com>
2018-10-02 22:46:15 -0400

    Added .click() and .dblclick() methods to <li> via jQuery.

--- scripts.js
+++ scripts.js
@@ -1,37 +1,42 @@
 // .ready() event handler method on document
 $(document).ready(() => {
-    // Dynamically creates <div class="row justify-content-center" id="row2"> and appends it to <div class="container">
-    $('.container').append('<div class="row justify-content-center" id="row2"></div>');
+    // Creates <div class="row">, appends to <div class="container">
+    $('.container').append('<div class="row justify-content-center" id="row3"></div>');
+    // Creates <ul>, appends to <div class="container">
+    $('#row3').append(`<ul class="list-group col-4 ml-3" id="myList"></ul>`);
 
     // .keyup() event handler method on #inputTxt
     $('#inputTxt').keyup(() => {
         if($('#inputTxt').val() !== '') {
             $('#btnSubmit').attr('disabled', false); // <input disabled="false">
-        };
-    });
+        }
+    })
 
     // .click() event handler method on #btnSubmit
     $('#btnSubmit').click(() => {
         if($('#inputTxt').val() !== '') {
-            // alert(`You have submitted "${$('#inputTxt').val()}"!`);
-            $('#row2').append(`<h2 class="col-8 text-center">${$('#inputTxt').val()}</h2>`)
-            $('#inputTxt').val(''); // Clears value
+            // Creates <li>, appends to #myList
+             $('#myList').append(`<li class="list-group-item">${$('#inputTxt').val()}</li>`)
+             $('#inputTxt').val(''); // Clear!
 
-            const myColors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#17a2b8', '#f8f9fa', '#343a40'];
+             const myColors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#17a2b8', '#f8f9fa', '#343a40'];
 
-            // Randomly selects an index from myColors, assigns value to myRandomColor
-            const getRandomColor = () => {
-                const myRandomColor = myColors[Math.floor(Math.random() * myColors.length)];
-                return myRandomColor;
-            };
+             // Randomly selects an index from myColors, assigns value to myRandomColor
+             const getRandomColor = () => {
+                 const myRandomColor = myColors[Math.floor(Math.random() * myColors.length)];
+                 return myRandomColor;
+             }
 
-            // .mouseover() event handler method on <h2>
-            $('h2').mouseover((event) => {
-                const thisRandomColor = getRandomColor();
-                $(event.target).css({
-                    'background-color': thisRandomColor,
-                    'border-radius': '0.25rem'});
-            });
-        };
-    });
-});
+             // .click() event handler method on <li>
+             $('li').click((event) => {
+                 const thisRandomColor = getRandomColor();
+                 $(event.target).css('background-color', thisRandomColor);
+            })
+
+            // .dblclick() event handler method on <li>
+            $('li').dblclick((event) => {
+                $(event.target).remove();
+            })
+        }
+    })
+})
